438a5e3208275abf08f46247c2f7d5667f39799f
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2021-01-04 05:08:40 +0000

    adding pyspark example with schema: work in progress..

---
+++ schema_example.py
@@ -0,0 +1,7 @@
+from pyspark.sql import SparkSession
+
+# define schema for our data using DDL
+schema = "`Id` INT, `First` STRING, `Last` STRING, `Url` STRING, 
+    `Published` STRING, `Hits` INT, `Campaigns` ARRAY<STRING>"
+
+# TODO: FInish this!
